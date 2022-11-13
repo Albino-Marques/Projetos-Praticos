0bcf0339559b4625241706dda9503fc16c006c50
Albino Marques <albinomarques01@gmail.com>
2022-11-12 23:22:26 -0300

    Feita as alterações no estilo novamente, bem como a inserção de interação por clice através do mouse nos botões e efeito 'expansivo' ao tocar algum dos sons.

--- 01-projeto/script.js
+++ 01-projeto/script.js
@@ -1,56 +1,53 @@
 //eventos: 'keyup' e 'keydown'
 
 /* Evento para "pegar" a tecla digitada. */
-document.body.addEventListener('keydown', (event)=>{
+document.body.addEventListener('keydown', (event) => {
     playSound(event.code.toLowerCase())
     console.log(event.code.toLowerCase())
 })
 
 /* Evento para "pegar" o click do mouse em "Reproduzir". */
-document.querySelector('.composer button').addEventListener('click',() => {
-    let song = document.querySelector('#input').value;
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value
     //let keyElement = document.querySelector(songArray)
-    
-    if (song !== ''){
-        let songArray = song.split('');
+
+    if (song !== '') {
+        let songArray = song.split('')
         playComposition(songArray)
     }
-
 })
 
 /* Evento para "pegar" o click do mouse nas teclas.   */
 
-
 /* Função para executar o som através do teclado numérico. */
 function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`);
+    let audioElement = document.querySelector(`#s_${sound}`)
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
-    if(audioElement) {
-        audioElement.currentTime = 0;
-        audioElement.play();
+    if (audioElement) {
+        audioElement.currentTime = 0
+        audioElement.play()
     }
 
-    if(keyElement){
+    if (keyElement) {
         keyElement.classList.add('active')
 
-        setTimeout(()=>{
+        setTimeout(() => {
             keyElement.classList.remove('active')
-        },200)
+        }, 200)
     }
 }
 
 /* Função para executar o som através da composição.*/
 
- function playComposition(songArray){
+function playComposition(songArray) {
     let wait = 0
 
-
-     for (let songItem of songArray){
-        setTimeout(()=>{
+    for (let songItem of songArray) {
+        setTimeout(() => {
             playSound(`numpad${songItem}`)
         }, wait)
 
         wait += 250
-     }
- }
+    }
+}
